70cafdda7a7554c5aa732d607cce3c948c9f7d4d
Aditya Deorha <aditya.deorha1@gmail.com>
2018-03-03 22:07:34 -0800

    Created a UsersService interface, which has an abstract function, createUser.

---
+++ src/main/java/com/appsdeveloperblog/app/ws/service/UsersService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appsdeveloperblog.app.ws.service;
+
+import com.appsdeveloperblog.app.ws.shared.dto.UserDTO;
+
+/**
+ *
+ * @author admin
+ */
+public interface UsersService {
+    public UserDTO createUser(UserDTO user);
+}
