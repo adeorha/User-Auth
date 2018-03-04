13bf7a90c2bf1c555fcdde88368b7fc0e2803cfc
Aditya Deorha <aditya.deorha1@gmail.com>
2018-03-03 22:06:49 -0800

    Created a UserDTO and using BeanUtils from Spring framework to copy the properties from the requested user object to the userDto.

--- src/main/java/com/appsdeveloperblog/app/ws/ui/entrypoints/UsersEntryPoint.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/entrypoints/UsersEntryPoint.java
@@ -5,6 +5,9 @@
  */
 package com.appsdeveloperblog.app.ws.ui.entrypoints;
 
+import com.appsdeveloperblog.app.ws.service.UsersService;
+import com.appsdeveloperblog.app.ws.service.impl.UsersServiceImpl;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDTO;
 import com.appsdeveloperblog.app.ws.ui.model.request.CreateUserRequestModel;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserProfileRest;
 import javax.ws.rs.Consumes;
@@ -12,6 +15,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.springframework.beans.BeanUtils;
 
 /**
  *
@@ -25,7 +29,17 @@ public class UsersEntryPoint {
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public UserProfileRest createUser(CreateUserRequestModel requestObject){
         UserProfileRest returnValue = new UserProfileRest();
+       
+        //Prepare UserDTO
+        UserDTO userDto = new UserDTO();
+        BeanUtils.copyProperties(requestObject, userDto);
         
+        //Create new user
+        UsersService userService = new UsersServiceImpl(); //Can use Spring Framework instead of this
+        UserDTO createdUserProfile = userService.createUser(userDto);
+        
+        //Prepare response
+        BeanUtils.copyProperties(createdUserProfile, returnValue);
         return returnValue;
     }
 }
