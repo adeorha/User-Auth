91ecec2acd6cfff617aeabc6a5b0d11caf9e4c38
Aditya Deorha <aditya.deorha1@gmail.com>
2018-03-09 11:26:03 -0800

    Sending verification email with random token and deployed it on AWS

--- src/main/java/com/appsdeveloperblog/app/ws/utils/AmazonSES.java
+++ src/main/java/com/appsdeveloperblog/app/ws/utils/AmazonSES.java
@@ -5,7 +5,8 @@
  */
 package com.appsdeveloperblog.app.ws.utils;
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
@@ -31,7 +32,7 @@ public class AmazonSES {
     final String HTMLBODY = "<h1>Please verify your email address</h1>"
             + "<p>Thank you for registering with our mobile app. To complete registration process and be able to log in,"
             + " click on the following link: "
-            + "<a href='ec2-18-188-30-205.us-east-2.compute.amazonaws.com/verify-email.jsp?token=$tokenValue'>"
+            + "<a href='http://ec2-18-188-30-205.us-east-2.compute.amazonaws.com:8080/MOBILE-APP-WS/verify-email.jsp?token=$tokenValue'>"
             + "Final step to complete your registration"
             + "</a><br/><br/>"
             + "Thank you! And we are waiting for you inside!";
@@ -40,52 +41,48 @@ public class AmazonSES {
     final String TEXTBODY = "Please verify your email address. "
             + "Thank you for registering with our mobile app. To complete registration process and be able to log in,"
             + " open then the following URL in your browser window: "
-            + " ec2-18-188-30-205.us-east-2.compute.amazonaws.com/verify-email.jsp?token=$tokenValue"
+            + " http://ec2-18-188-30-205.us-east-2.compute.amazonaws.com:8080/MOBILE-APP-WS/verify-email.jsp?token=$tokenValue"
             + " Thank you! And we are waiting for you inside!";
 
-    
+    public void verifyEmail(UserDTO userDto) {
+        try {
+            BasicAWSCredentials credentials = new BasicAWSCredentials("*******", "****");
+            AmazonSimpleEmailService client
+                    = AmazonSimpleEmailServiceClientBuilder.standard()
+                            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                            .withRegion(Regions.US_WEST_2).build();
 
-  public void verifyEmail(UserDTO userDto)
-  {
-      try {
-      AmazonSimpleEmailService client = 
-          AmazonSimpleEmailServiceClientBuilder.standard()
-            .withCredentials(new ProfileCredentialsProvider())
-            .withRegion(Regions.US_WEST_2).build();
-      
-      String htmlBodyWithToken = HTMLBODY.replace("$tokenValue", userDto.getEmailVerificationToken());
-      String textBodyWithToken = TEXTBODY.replace("$tokenValue", userDto.getEmailVerificationToken());
-      
-      SendEmailRequest request = new SendEmailRequest()
-          .withDestination(
-              new Destination().withToAddresses( userDto.getEmail() ) )
-          .withMessage(new Message()
-              .withBody(new Body()
-                  .withHtml(new Content()
-                      .withCharset("UTF-8").withData(htmlBodyWithToken))
-                  .withText(new Content()
-                      .withCharset("UTF-8").withData(textBodyWithToken)))
-              .withSubject(new Content()
-                  .withCharset("UTF-8").withData(SUBJECT)))
-          .withSource(FROM);
+            String htmlBodyWithToken = HTMLBODY.replace("$tokenValue", userDto.getEmailVerificationToken());
+            String textBodyWithToken = TEXTBODY.replace("$tokenValue", userDto.getEmailVerificationToken());
 
-      client.sendEmail(request);
-      
-      System.out.println("Email sent!");
-      
-    } catch (Exception ex) {
-        throw new EmailVerificationException(ex.getMessage());
+            SendEmailRequest request = new SendEmailRequest()
+                    .withDestination(
+                            new Destination().withToAddresses(userDto.getEmail()))
+                    .withMessage(new Message()
+                            .withBody(new Body()
+                                    .withHtml(new Content()
+                                            .withCharset("UTF-8").withData(htmlBodyWithToken))
+                                    .withText(new Content()
+                                            .withCharset("UTF-8").withData(textBodyWithToken)))
+                            .withSubject(new Content()
+                                    .withCharset("UTF-8").withData(SUBJECT)))
+                    .withSource(FROM);
 
+            client.sendEmail(request);
+
+            System.out.println("Email sent!");
+
+        } catch (Exception ex) {
+            throw new EmailVerificationException(ex.getMessage());
+
+        }
     }
-  }
-  
-  
- public static void main(String[] args)
-  {
-      UserDTO userDto = new UserDTO();
-      userDto.setEmail("anchu.khanna@gmail.com");
-      userDto.setEmailVerificationToken("fhf736djslfi4823kj");
-      new AmazonSES().verifyEmail(userDto);
-  }
-  
+
+    public static void main(String[] args) {
+        UserDTO userDto = new UserDTO();
+        userDto.setEmail("anchu.khanna@gmail.com");
+        userDto.setEmailVerificationToken("fhf736djslfi4823kj");
+        new AmazonSES().verifyEmail(userDto);
+    }
+
 }
